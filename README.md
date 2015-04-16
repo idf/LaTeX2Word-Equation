708da3abe57209592d3a01f07b9c2d1fb415fd13
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-04-16 18:09:00 +0800

    JaxToML

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ LaTex Equation to Word Equation
 TODO: need a gif
 
 # Flow 
-Copy LaTex -> MathML Code -> Word  
+Copy LaTex -> MathML Code -> Word
+
+[MathJax](https://www.mathjax.org/)
 [ref](http://tex.stackexchange.com/questions/25223/embed-latex-math-equations-into-microsoft-word)
--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 var bkg = chrome.extension.getBackgroundPage();
 var alt = "";
+// access MathJax through CDN
+$('script').append('<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"></script>');
 
 var storeAlt = function(e) {
     alt = e.alt;
@@ -15,15 +17,39 @@ var copyToClipBoard = function(text) {
     input.remove();
 };
 
-var toMathMarkUp = function(text) {
-    return "";
+var JaxToML = {
+    toMathML: function(jax, callback) {
+        var mml;
+        try {
+            mml = jax.root.toMathML("");
+        } catch (err) {
+            if (!err.restart) {
+                throw err
+            } // an actual error
+            return MathJax.Callback.After([JaxToML.toMathML, jax, callback], err.restart);
+        }
+        MathJax.Callback(callback)(mml);
+    },
+    convert: function(AjaxText, callback) {
+        var tempDiv = $('<div style="width:455px;height:450px:border-width:thick;border-style:double;"></div>').appendTo("body").html(AjaxText)[0];
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, tempDiv]); //first place in Q
+        MathJax.Hub.Queue(function() { //wait for a callback to be fired
+            var jax = MathJax.Hub.getAllJax(tempDiv);
+            for (var i = 0; i < jax.length; i++) {
+                JaxToML.toMathML(jax[i], function(mml) {//alert(jax[i].originalText + "\n\n=>\n\n"+ mml);
+                    AjaxText = AjaxText.replace(jax[i].originalText, mml);
+                });
+            }
+            $(tempDiv).remove();
+            AjaxText = AjaxText.replace(/\(/g,""); //notice this escape character for ( - i.e it has to be \( , know why it is beacuse JS will treat ) or ( as end/begin of function as there are no quotes here.
+            AjaxText = AjaxText.replace(/\)/g,""); //notice this escape character for ) - i.e it has to be \)
+            AjaxText = AjaxText.replace(/\\/g,"");
+            callback(AjaxText);
+        });
+    }
 };
 
-
 var clickHandler = function(e) {
-    bkg.console.log(e);
-    var buzzPostUrl = "http://www.google.com/buzz/post?";
-
     var text;
     if (e.selectionText) {
         text = e.selectionText;
@@ -32,9 +58,10 @@ var clickHandler = function(e) {
     if (e.mediaType==="image") {
         text = alt;
     }
-
-    bkg.console.log(text);
-    copyToClipBoard(text);
+    JaxToML.convert("$$\n"+text+"\n$$", function(mml) {
+        mml = mml.replace(/^\$+|\$+$/g, '');
+        copyToClipBoard(mml);
+    });
 };
 
 chrome.extension.onRequest.addListener(storeAlt);
